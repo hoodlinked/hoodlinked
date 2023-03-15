40b2390a60c7cae25bc7ca1c1a8182cf4be41548
genmla <gmmladenka@gmail.com>
2023-03-14 19:36:00 -0500

    added home page to test heroku deployment

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,55 @@
 import logo from './logo.svg';
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import {
+  ApolloClient,
+  InMemoryCache,
+  ApolloProvider,
+  createHttpLink,
+} from '@apollo/client';
+import { setContext } from '@apollo/client/link/context';
+
 import './App.css';
+
 import Nav from './components/Nav';
+import Home from './Pages/Home'
+import LoginForm from './components/LoginForm'
+
+const httpLink = createHttpLink({
+  uri: '/graphql',
+});
+
+const authLink = setContext((_, { headers }) => {
+  const token = localStorage.getItem('id_token');
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : '',
+    },
+  };
+});
+
+const client = new ApolloClient({
+  link: authLink.concat(httpLink),
+  cache: new InMemoryCache(),
+});
 
 function App() {
   return (
-    <div >
-    <Nav />
-    </div>
+    <ApolloProvider client={client}>
+        <Router>
+          <div >
+            <Nav />
+            <Routes>
+              <Route
+                path="/"
+                element={<Home />}
+              />
+            </Routes>
+          </div>
+        </Router>
+    </ApolloProvider>
+
   );
 }
 
--- client/src/Pages/Home.js
+++ client/src/Pages/Home.js
@@ -1,7 +1,24 @@
-import React from 'react'
+import { 
+  Heading, 
+  Box,
+  Text,
+  Button,
+} from '@chakra-ui/react'
 
 export default function Home() {
   return (
-    <div>Home</div>
+    <div>
+<Box maxW='32rem'>
+      <Heading mb={4}>Modern online and offline payments for Africa</Heading>
+      <Text fontSize='xl'>
+        Paystack helps businesses in Africa get paid by anyone, anywhere in the
+        world
+      </Text>
+      <Button size='lg' colorScheme='green' mt='24px'>
+        Create a free account
+      </Button>
+    </Box>
+    </div>
+    
   )
 }
