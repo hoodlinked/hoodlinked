68b4d0e78324abcdf8462b8170654a532b59d903
genmla <gmmladenka@gmail.com>
2023-03-14 20:10:16 -0500

    added login and signup form to homepage

--- client/src/Pages/Home.js
+++ client/src/Pages/Home.js
@@ -1,24 +1,26 @@
-import { 
-  Heading, 
+import {
+  Heading,
   Box,
   Text,
-  Button,
 } from '@chakra-ui/react'
+
+import LoginForm from '../components/LoginForm'
+import SignUp from '../components/SignUp'
+
+
 
 export default function Home() {
   return (
     <div>
-<Box maxW='32rem'>
-      <Heading mb={4}>Modern online and offline payments for Africa</Heading>
-      <Text fontSize='xl'>
-        Paystack helps businesses in Africa get paid by anyone, anywhere in the
-        world
-      </Text>
-      <Button size='lg' colorScheme='green' mt='24px'>
-        Create a free account
-      </Button>
-    </Box>
+      <Box maxW='32rem'>
+        <Heading mb={4}>Welcome to Hoodlinked!</Heading>
+        <Text fontSize='xl'>
+          A tool-share app for sharing items with your communities
+        </Text>
+      </Box>
+      <LoginForm />
+      <SignUp />
     </div>
-    
+
   )
 }
