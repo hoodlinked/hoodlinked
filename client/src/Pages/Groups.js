e8b6afc7dc07d7adecd5557a8ad4fc25752141d5
mintedd <xxxxiheartyouxxxxx@gmail.com>
2023-03-17 22:23:40 -0400

    cards rednering with map

--- client/src/Pages/Groups.js
+++ client/src/Pages/Groups.js
@@ -1,39 +1,67 @@
 import {
-  Heading,
-  Box,
-  Text,
-  Stack
+    Container, Card, Text, CardBody, CardFooter, Image, Stack, Heading, Button,
+    Flex, SimpleGrid
 } from '@chakra-ui/react'
 
-import Cards from '../components/Cards'
+// import React, { useState } from 'react';
 
+import {  useQuery } from '@apollo/client';
 
-export default function Dashboard() {
-  return (
-    <>
-      <Box>
-      <Stack spacing="6" align="center">
-        <Box margin="1rem">
-          <Heading 
-          mb={4} 
-          fontFamily="Rubik Iso" 
-          fontWeight="extrabold">
-            Welcome to Groups!
-          </Heading>
-          <Text fontSize='xl' align="center">
-            Current open Communities
-          </Text>
-      </Box>
+import { QUERY_LIBRARIES } from "../utils/queries";
 
-      <Box w="100%">
-        <Cards />
-      </Box>
-    </Stack>
-  </Box>
+function Cards() {
+    const { data } = useQuery(QUERY_LIBRARIES);
+    let library;
 
-    </>
+    if (data) {
+        library = data.libraries;
+        console.log(library)
+    }
+    return (
+        <Container>
+            <Flex>
+                <SimpleGrid>
+                    {library ? (
+                        <>
+                            {library.map(({ name }, index) => (
+                                <Card>
+                                    <Heading bg="white" margin="1rem 0" borderWidth="1px" borderColor="gray.200" borderRadius="lg" p="4" width={{ base: "100%", sm: "48%", md: "30%" }}>
 
+                                        <div key={index} className="my-2">
+                                            <p>{name}</p>
+                                        </div>
+                                    </Heading>
+                                    <Image
+                                        objectFit='cover'
+                                        maxW={{ base: '100%', sm: '200px' }}
+                                        src='https://images.unsplash.com/photo-1667489022797-ab608913feeb?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHw5fHx8ZW58MHx8fHw%3D&auto=format&fit=crop&w=800&q=60'
+                                        alt='Caffe Latte'
+                                    />
+                                    <Stack align='center' >
+                                        <CardBody >
 
+                                            <Text py='2'>
+                                                Maybe put member(user) count here?
+                                            </Text>
+                                        </CardBody>
 
-  )
+                                        <CardFooter>
+                                            <Button variant='solid' colorScheme='blue'>
+                                                <a href="mailto:xxxxiheartyouxxxxx@example.com">Join this Group</a>
+                                            </Button>
+                                        </CardFooter>
+                                    </Stack>
+                                </Card>
+                            ))}
+                        </>
+                    ) :
+                        null
+                    }
+                </SimpleGrid>
+            </Flex>
+        </Container >
+    )
 }
+
+
+export default Cards;
