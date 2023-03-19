c127992d33a33b383f2e73443c1e50f40ed1371f
Jennifer Arboleda <arboleda.jen@gmail.com>
2023-03-18 19:55:33 -0500

    Moved welcome user message to nav. Fixed dashboard responsiveness.

--- client/src/Pages/Dashboard.js
+++ client/src/Pages/Dashboard.js
@@ -7,6 +7,7 @@ import {
 
 import Item from '../components/Item';
 import React, { useState, useRef, useEffect } from "react";
+import { useBreakpointValue } from '@chakra-ui/react';
 import AddLibrary from '../components/AddLibrary';
 import Home from '../Pages/Home'
 import LoginForm from '../components/LoginForm'
@@ -21,6 +22,7 @@ import Auth from '../utils/auth';
 export default function Dashboard() {
 
   const { data } = useQuery(QUERY_USER);
+  const isLargerThan768 = useBreakpointValue({ base: false, md: true });
 
   const [showSignUpForm, setShowSignUpForm] = useState(false);
   const [showLoginForm, setShowLoginForm] = useState(false);
@@ -43,30 +45,45 @@ export default function Dashboard() {
 
   return (
     <>
+
       {user ? (
         <Flex direction="row" height="100%">
-          <Box w="25%" bg="gray.100">
-            <Box p="4">
-              <Avatar size="lg" src={user.avatarUrl} />
-              <Heading mt="4" mb="2">Hello, {user.username}!</Heading>
-              <Text fontSize="xl">{user.email}</Text>
-            </Box>
-          </Box>
-          <Box w="75%" bg="white" p="4">
-            <Heading mb="4">My Items</Heading>
-            <Item />
-            <Divider margin="3rem 0" />
-            <Heading mb="4">My Libraries</Heading>
-            <AddLibrary />
-          </Box>
+          {!isLargerThan768 && (
+            <>
+              <Box w="100%" bg="white" p="4">
+                <Heading mb="4">My Items</Heading>
+                <Item />
+                <Divider margin="3rem 0" />
+                <Heading mb="4">My Libraries</Heading>
+                <AddLibrary />
+              </Box>
+            </>    
+          )}
+
+          {isLargerThan768 && (
+            <>
+              <Box w="25%" bg="orange.100" minW="fit-content">
+                <Box p="4">
+                  <Avatar size="lg" src={user.avatarUrl} />
+                  <Heading size="md" mt="4" mb="2">Hello,</Heading>
+                  <Heading>{user.username}!</Heading>
+                  <Text fontSize="md">{user.email}</Text>
+                </Box>
+              </Box>
+              <Box w="75%" bg="white" p="4">
+                <Heading mb="4">My Items</Heading>
+                <Item />
+                <Divider margin="3rem 0" />
+                <Heading mb="4">My Libraries</Heading>
+                <AddLibrary />
+              </Box>
+            </>
+          )}
         </Flex>
-
       ) : (
-
-        <>
         <Home />
-        </>
       )}
+
 
     </>
   )
