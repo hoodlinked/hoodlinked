60d29087c9f190756d77d0058265ae536c7f2832
Jennifer Arboleda <arboleda.jen@gmail.com>
2023-03-18 19:02:09 -0500

    Updated home page

--- client/src/Pages/Home.js
+++ client/src/Pages/Home.js
@@ -8,7 +8,7 @@ import {
   Button
 
 } from '@chakra-ui/react'
-import { Container } from '@chakra-ui/react'
+import { useBreakpointValue } from '@chakra-ui/react'
 import React, { useState , useRef, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 import LoginForm from '../components/LoginForm'
@@ -21,6 +21,7 @@ import { QUERY_USER } from "../utils/queries";
 export default function Home() {
   // const navigate = useNavigate();
   const { data } = useQuery(QUERY_USER);
+  const isLargerThan768 = useBreakpointValue({ base: false, md: true });
   
   const [showSignUpForm, setShowSignUpForm] = useState(false);
   const [showLoginForm, setShowLoginForm] = useState(false);
@@ -45,7 +46,7 @@ export default function Home() {
   return (
     <>
       {/* logic for showing the cards page - with context has option for user to join/view library page */}
-      
+
       {user ? (
         <Box>
           <Heading mb={4}>You're already logged in!</Heading>
@@ -57,34 +58,67 @@ export default function Home() {
         </Box>
       ) : (
         <>
-        <Flex flexDirection="column" alignItems="center" my="4rem">
-            <Image src="hoodlinked-logo.png" alt="Hoodlinked Logo" width="200px" height="200px" mb="2rem" />
-            <Text fontSize="3xl" fontWeight="bold" mb="2rem" textAlign="center">
-              Welcome to Hoodlinked!
-            </Text>
-            <Text fontSize="xl" mb="2rem" textAlign="center">
-              Hoodlinked is a community-based sharing app that allows you to share and exchange items with people in your local area. Join a local group and start sharing today!
-            </Text>
-            <Button onClick={() => setShowSignUpForm(!showSignUpForm)} variant="solid" colorScheme="orange" margin="1rem 0">
-              {showSignUpForm ? 'Hide form' : 'Sign up for free'}
-            </Button>
-              {showSignUpForm && (
-              <SignUp />
-            )}
 
-            <Link onClick={() => setShowLoginForm(!showLoginForm)} fontSize="lg" color="gray.500" mb="2rem">
-              Already have an account? Log in
-            </Link>
-            {showLoginForm && (
-              <LoginForm />
-            )}
-
+      {!isLargerThan768 && (
+        <>
+          <Flex>
+          <Image src="/assets/PastelHero.png" alt="Hero Image" width="100%" />
           </Flex>
+          <Flex flexDirection="column" alignItems="center" my="4rem">
+          
+          <Text fontSize="3xl" fontWeight="bold" mb="2rem" textAlign="center">
+            Welcome to Hoodlinked!
+          </Text>
+          <Text fontSize="xl" mb="2rem" textAlign="center" px={[8, 16]}>
+            Hoodlinked is a community-based sharing app that allows you to share and exchange items with people in your local area. Join a local group and start sharing today!
+          </Text>
+          <Button onClick={() => setShowSignUpForm(!showSignUpForm)} variant="solid" colorScheme="orange" margin="1rem 0">
+            {showSignUpForm ? 'Hide form' : 'Sign up for free'}
+          </Button>
+            {showSignUpForm && (
+            <SignUp />
+          )}
 
-
+          <Link onClick={() => setShowLoginForm(!showLoginForm)} fontSize="lg" color="gray.500" mb="2rem">
+            Already have an account? Log in
+          </Link>
+          {showLoginForm && (
+            <LoginForm />
+          )}
+        </Flex>
         </>
       )}
-      
+
+      {isLargerThan768 && (
+        <>
+        <Flex flexDirection="column" alignItems="center" my="4rem">
+        <Text fontSize="3xl" fontWeight="bold" mb="2rem" textAlign="center">
+          Welcome to Hoodlinked!
+        </Text>
+        <Text fontSize="xl" mb="2rem" textAlign="center" px={[12, 20]}>
+          Hoodlinked is a community-based sharing app that allows you to share and exchange items with people in your local area. Join a local group and start sharing today!
+        </Text>
+        <Button onClick={() => setShowSignUpForm(!showSignUpForm)} variant="solid" colorScheme="orange" margin="1rem 0">
+          {showSignUpForm ? 'Hide form' : 'Sign up for free'}
+        </Button>
+          {showSignUpForm && (
+          <SignUp />
+        )}
+
+        <Link onClick={() => setShowLoginForm(!showLoginForm)} fontSize="lg" color="gray.500" mb="2rem">
+          Already have an account? Log in
+        </Link>
+        {showLoginForm && (
+          <LoginForm />
+        )}
+        </Flex>
+        <Flex>
+        <Image src="/assets/PastelHero.png" alt="Hero Image" width="100%" />
+        </Flex>
+        </>
+        )}
+      </>
+    )}  
     </>
   );
 }
